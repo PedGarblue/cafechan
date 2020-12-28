ce9a188045cf6f9bfda46d9afe68bce017677721
PedGarblue <pedro.alejandro.g@hotmail.com>
2020-12-28 15:04:18 -0400

    Added test cases for client panel login

--- tests/client/fixtures/store.fixture.js
+++ tests/client/fixtures/store.fixture.js
@@ -1,4 +1,6 @@
-const moment = require('moment');
+import moment from 'moment';
+import Vuex from 'vuex';
+import { shallowMount } from '@vue/test-utils';
 
 const storeFixture = {
   state: {
@@ -18,6 +20,19 @@ const storeFixture = {
   commit: jest.fn(),
 };
 
+const createWrapper = (component, localVue, overrides) => {
+  const defaults = {
+    localVue,
+    store: new Vuex.Store({}),
+    mocks: {
+      $router: {},
+      $route: {},
+    },
+  };
+  return shallowMount(component, Object.assign(defaults, overrides));
+};
+
 module.exports = {
   storeFixture,
+  createWrapper,
 };
